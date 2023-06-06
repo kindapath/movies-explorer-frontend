6eed9520fdfa057df489e4bf099d84a1137af939
Zhenya Nikolaev <kindaboii@yandex.ru>
2023-06-06 16:27:08 +0300

    feat: realize routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import AboutProject from '../AboutProject/AboutProject';
+import Login from '../Login/Login';
+import Movies from '../Movies/Movies';
+import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+
+
+      {/* routes */}
+      <Routes>
+        <Route path='/' element={<AboutProject />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+      </Routes>
     </div>
   );
 }
